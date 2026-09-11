26ac2e0ca2f5d7045e838f91b3b171f211f66ce3
David Martinez <19002218@alumnos.utleon.edu.mx>
2026-09-10 21:03:13 -0600

    Heroes 10/09/2026

--- src/app/app.ts
+++ src/app/app.ts
@@ -7,5 +7,16 @@ import { Component, signal } from '@angular/core';
   styleUrl: './app.css'
 })
 export class App {
-  protected readonly title = signal('introAngular');
+   title:string = 'Edgar David Martinez Gonzalez'
+
+   duplicarnumero(a:number):number{
+    return a*2
+   }
+
+   pelicula={
+    titulo:'la que te tragas',
+    fechaLanzamiento: new Date(),
+    precio: 2353
+   }
+
 }
